54c290b33cdd07b7f47678515fd6ee7e4af4efca
Pl4yingNight <pl4yingnight@gmail.com>
2020-09-11 22:55:40 +0200

    [java-utils-async] deployed version 2.0.2 + extended Trigger support

--- java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/Trigger.java
+++ java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/Trigger.java
@@ -3,10 +3,16 @@ package net.runeduniverse.libs.utils.async;
 import java.util.concurrent.Semaphore;
 
 public class Trigger extends AChain<Trigger> {
-	private Semaphore semaphore = new Semaphore(0);
+	private final Semaphore semaphore;
 
 	public Trigger() {
 		instance = this;
+		this.semaphore = new Semaphore(0, true);
+	}
+
+	public Trigger(int permits) {
+		instance = this;
+		this.semaphore = new Semaphore(permits, true);
 	}
 
 	@Override
--- java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/TriggerMap.java
+++ java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/TriggerMap.java
@@ -4,15 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TriggerMap<KEY> {
+
+	private final int initPermits;
 	private Map<KEY, Trigger> map = new HashMap<>();
+
+	public TriggerMap() {
+		this.initPermits = 0;
+	}
+
+	public TriggerMap(int initPermits) {
+		this.initPermits = initPermits;
+	}
 
 	public Trigger create(KEY key) {
 		Trigger trigger = this.map.get(key);
 		if (trigger != null)
 			return trigger;
-		trigger = new Trigger();
+		trigger = new Trigger(this.initPermits);
 		this.map.put(key, trigger);
 		return trigger;
+	}
+
+	public boolean containsKey(KEY key) {
+		return this.map.containsKey(key);
+	}
+
+	public boolean containsTrigger(Trigger trigger) {
+		return this.map.containsValue(trigger);
 	}
 
 	public void remove(KEY key) {
