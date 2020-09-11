2f81a1576c158a0adb67e4187e8b977875068b59
Pl4yingNight <pl4yingnight@gmail.com>
2020-09-11 22:16:00 +0200

    [java-utils-async] deployed version 2.0.1 + extended TriggerMap

--- java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/TriggerMap.java
+++ java-utils-async/src/main/java/net/runeduniverse/libs/utils/async/TriggerMap.java
@@ -3,19 +3,35 @@ package net.runeduniverse.libs.utils.async;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TriggerMap {
-	private Map<Object, Trigger> map = new HashMap<>();
+public class TriggerMap<KEY> {
+	private Map<KEY, Trigger> map = new HashMap<>();
 
-	public Trigger create(Object key) {
-		Trigger trigger = new Trigger();
+	public Trigger create(KEY key) {
+		Trigger trigger = this.map.get(key);
+		if (trigger != null)
+			return trigger;
+		trigger = new Trigger();
 		this.map.put(key, trigger);
 		return trigger;
 	}
 
-	public void trigger(Object key) {
+	public void remove(KEY key) {
+		this.map.remove(key);
+	}
+
+	public void clear() {
+		this.map.clear();
+	}
+
+	public void trigger(KEY key) {
 		Trigger trigger = this.map.get(key);
 		if (trigger == null)
 			return;
 		trigger.trigger();
+	}
+
+	public void triggerAll() {
+		for (KEY key : this.map.keySet())
+			this.trigger(key);
 	}
 }
