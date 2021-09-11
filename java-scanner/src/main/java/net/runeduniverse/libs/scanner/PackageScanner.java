cf8f0ada478b4cc69823bde66ad2545feda68cd2
Pl4yingNight <pl4yingnight@gmail.com>
2021-09-11 16:22:18 +0200

    [java-scanner] fixed debug output

--- java-scanner/src/main/java/net/runeduniverse/libs/scanner/PackageScanner.java
+++ java-scanner/src/main/java/net/runeduniverse/libs/scanner/PackageScanner.java
@@ -97,8 +97,10 @@ public class PackageScanner {
 
 		DataMap<Class<?>, ClassLoader, String> classes = new DataHashMap<>();
 		Intercepter i = new Intercepter("PackageScanner INFO", this.debug);
-		IIntercepter iPkg = i.addSection("URL", "Discovered URL's", StringListBuilder.DEFAULT_LIST_INDENT);
-		IIntercepter iClass = i.addSection("CLASS", "Classes", StringListBuilder.DEFAULT_LIST_INDENT);
+		IIntercepter iPkg = i.addSection("URL", "Discovered URL's",
+				StringListBuilder.DEFAULT_INDENT + StringListBuilder.DEFAULT_LIST_INDENT);
+		IIntercepter iClass = i.addSection("CLASS", "Classes",
+				StringListBuilder.DEFAULT_INDENT + StringListBuilder.DEFAULT_LIST_INDENT);
 		synchronized (this.loader) {
 			for (ClassLoader classLoader : loader)
 				for (String pkg : pkgs)
