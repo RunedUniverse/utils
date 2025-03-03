96c099b270ce19497276ed9d2bcad4ca98b45ea7
VenaNocta <venanocta@gmail.com>
2025-03-03 17:29:52 +0100

    [java-utils-common] added more CollectionUtils

--- java-utils-common/src/main/java/net/runeduniverse/lib/utils/common/CollectionUtils.java
+++ java-utils-common/src/main/java/net/runeduniverse/lib/utils/common/CollectionUtils.java
@@ -16,19 +16,109 @@
 package net.runeduniverse.lib.utils.common;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.function.Supplier;
 
 public class CollectionUtils {
 
-	public static final <T> T first(Collection<T> collection) {
+	public static final <T> T first(final Collection<T> collection) {
 		for (T t : collection)
 			return t;
 		return null;
 	}
 
-	public static final <T> T firstNotNull(Collection<T> collection) {
+	public static final <T> T firstNotNull(final Collection<T> collection) {
 		for (T t : collection)
 			if (t != null)
 				return t;
 		return null;
+	}
+
+	public static final <T> T last(final Collection<T> collection) {
+		if (collection instanceof Deque) {
+			final Deque<T> deque = (Deque<T>) collection;
+			return deque.getLast();
+		}
+		for (Iterator<T> i = collection.iterator(); i.hasNext();) {
+			boolean next = i.hasNext();
+			final T obj = (T) i.next();
+			if (!next)
+				return obj;
+		}
+		return null;
+	}
+
+	public static final <T> T lastNotNull(final Collection<T> collection) {
+		// check for deque as it can be efficiently interated backwards
+		if (collection instanceof Deque) {
+			final Deque<T> deque = (Deque<T>) collection;
+			for (Iterator<T> i = deque.descendingIterator(); i.hasNext();) {
+				final T obj = (T) i.next();
+				if (obj != null)
+					return obj;
+			}
+			return null;
+		}
+		// otherwise search everything
+		T value = null;
+		for (Iterator<T> i = collection.iterator(); i.hasNext();) {
+			boolean next = i.hasNext();
+			final T obj = (T) i.next();
+			if (obj != null)
+				value = obj;
+			if (!next)
+				return obj;
+		}
+		return value;
+	}
+
+	public static final <T> List<T> copy(final List<T> src, final Supplier<List<T>> supplier) {
+		final List<T> lst = supplier.get();
+		lst.addAll(src);
+		return lst;
+	}
+
+	public static final <T> Set<T> copy(final Set<T> src, final Supplier<Set<T>> supplier) {
+		final Set<T> set = supplier.get();
+		set.addAll(src);
+		return set;
+	}
+
+	public static final <K, V> Map<K, V> copy(final Map<K, V> src, final Supplier<Map<K, V>> supplier) {
+		final Map<K, V> map = supplier.get();
+		for (Entry<K, V> entry : src.entrySet()) {
+			map.put(entry.getKey(), entry.getValue());
+		}
+		return map;
+	}
+
+	public static final <T> List<T> unmodifiable(final List<T> src) {
+		return Collections.unmodifiableList(src);
+	}
+
+	public static final <T> Set<T> unmodifiable(final Set<T> src) {
+		if (src instanceof NavigableSet)
+			return Collections.unmodifiableNavigableSet((NavigableSet<T>) src);
+		if (src instanceof SortedSet)
+			return Collections.unmodifiableSortedSet((SortedSet<T>) src);
+		return Collections.unmodifiableSet(src);
+	}
+
+	public static final <K, V> Map<K, V> unmodifiable(final Map<K, V> src) {
+		if (src instanceof NavigableMap)
+			return Collections.unmodifiableNavigableMap((NavigableMap<K, V>) src);
+		else if (src instanceof SortedMap)
+			return Collections.unmodifiableSortedMap((SortedMap<K, V>) src);
+		return Collections.unmodifiableMap(src);
 	}
 }
