c8148831d5b08d7690105092ee0b09a620d63dc4
VenaNocta <venanocta@gmail.com>
2025-03-14 14:26:05 +0100

    [java-utils-errors] refactored pkg paths

--- java-utils-errors/src/main/java/net/runeduniverse/lib/utils/error/ATrunkableException.java
+++ java-utils-errors/src/main/java/net/runeduniverse/lib/utils/error/ATrunkableException.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.runeduniverse.lib.utils.errors;
+package net.runeduniverse.lib.utils.error;
 
 import java.util.ArrayList;
 import java.util.List;
--- java-utils-errors/src/test/java/net/runeduniverse/lib/utils/error/test/package-info.java
+++ java-utils-errors/src/test/java/net/runeduniverse/lib/utils/error/test/package-info.java
@@ -13,28 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.runeduniverse.lib.utils.errors;
-
-import java.util.Collection;
-
-public class ExceptionSuppressions extends ATrunkableException {
-	private static final long serialVersionUID = 215607527815606000L;
-
-	public ExceptionSuppressions(String message) {
-		super(message);
-	}
-
-	public ExceptionSuppressions(String message, boolean trunk) {
-		super(message, trunk);
-	}
-
-	public ExceptionSuppressions addSuppressed(Collection<Exception> errors) {
-		for (Exception e : errors)
-			this.addSuppressed(e);
-		return this;
-	}
-
-	public boolean hasSuppressions() {
-		return this.getSuppressed().length > 0;
-	}
-}
+package net.runeduniverse.lib.utils.error.test;
